88cada816f10df802c3a0684442cd973232fe2b8
Tiu, Gregory Alan <gtiu@gatech.edu>
2023-02-12 17:52:06 -0500

    init

---
+++ Ebay_Hunter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ebay_Hunter
+//
+//  Created by School on 2/12/23.
+//  Copyright © 2023 Black Spaniel Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
